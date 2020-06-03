53cf326becac55ce6ab3819e3c0bf235ab1b2aea
Bria <54779367+BriaThomas21@users.noreply.github.com>
2020-06-03 18:31:11 -0400

    Added site title, and page links

--- abc_website/views.py
+++ abc_website/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render #This allows you to render the template
 
 #Function that takes an http request and returns the html template associated with the request
 def index(request):
-    message = "Hello, you are now at the Aspiring Black Coders home page!"
+    message = "Aspiring Black Coders"
     template_name = "index.html"
     context = {'message': message} #Can be displayed using double curly brace notation in your html template
     return render(request, template_name, context)
 
 
 def mission(request):
-    message = "This is ABC's mission statement."
+    message = "A community for people of color aspiring to begin and grow careers in tech together. "
     statement = "Insert mission statement below."
     template_name = "mission.html"
     context = {'message': message, 'statement': statement}
