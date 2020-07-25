926802ab8c85afa7c4a6fab60df394c1353b1f8c
Barry Johnson <bdotmgmt@gmail.com>
2020-07-25 14:12:48 -0400

    Styled job_board.html

--- job_board/views.py
+++ job_board/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def get_jobs(request):
-    url = 'https://www.themuse.com/api/public/jobs?level=Entry%20Level&level=Mid%20Level&location=Tampa%2C%20FL&page=1&descending=true'
+    url = 'https://www.themuse.com/api/public/jobs?level=Entry%20Level&level=Mid%20Level&location=Tampa%2C%20FL&page=2&descending=true'
     x = requests.get(url)
     jobs = x.json()['results']
 
