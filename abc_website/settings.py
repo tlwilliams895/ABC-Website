65dd2c753350f2972a4325b457f8273c306ac5d6
Barry Johnson <bdotmgmt@gmail.com>
2020-11-03 15:17:57 -0500

    Made adjustment to settings file

--- abc_website/settings.py
+++ abc_website/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'abc_website/static'),
+    os.path.join(BASE_DIR, '/static/'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
